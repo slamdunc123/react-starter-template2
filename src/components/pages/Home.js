e79a269fd2f8b4e88598d52e7bf3f45c825c9019
Duncan Laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-12-31 11:06:51 +0000

    created responsive page template for home page using sections and articles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,84 @@
 import React from 'react'
 
+// styles 
+import './Home.scss';
+
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home-container">
+			<section className='home-section'>
+				<article className='home-article' id='home-article__one'>
+					<div className='home-article__heading'>
+						<h4>Home Article Heading 1</h4>
+					</div>
+					<div className='home-article__body'>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Temporibus est laborum molestiae! Sequi voluptas velit vitae ea
+							possimus recusandae? Nemo reiciendis laudantium consequatur fuga,
+							officiis facere qui dolorem quia earum?
+						</p>
+					</div>
+				</article>
+				<article className='home-article' id='home-article__two'>
+					<div className='home-article__heading'>
+						<h4>Home Article Heading 2</h4>
+					</div>
+					<div className='home-article__body'>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Temporibus est laborum molestiae! Sequi voluptas velit vitae ea
+							possimus recusandae? Nemo reiciendis laudantium consequatur fuga,
+							officiis facere qui dolorem quia earum?
+						</p>
+					</div>
+				</article>
+			</section>
+
+			<section className='home-section'>
+				<article className='home-article' id='home-article__three'>
+					<div className='home-article__heading'>
+						<h4>Home Article Heading 3</h4>
+					</div>
+					<div className='home-article__body'>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Temporibus est laborum molestiae! Sequi voluptas velit vitae ea
+							possimus recusandae? Nemo reiciendis laudantium consequatur fuga,
+							officiis facere qui dolorem quia earum?
+						</p>
+					</div>
+				</article>
+			</section>
+
+			<section className='home-section'>
+				<article className='home-article' id='home-article__four'>
+					<div className='home-article__heading'>
+						<h4>Home Article Heading 4</h4>
+					</div>
+					<div className='home-article__body'>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Temporibus est laborum molestiae! Sequi voluptas velit vitae ea
+							possimus recusandae? Nemo reiciendis laudantium consequatur fuga,
+							officiis facere qui dolorem quia earum?
+						</p>
+					</div>
+				</article>
+				<article className='home-article' id='home-article__five'>
+					<div className='home-article__heading'>
+						<h4>Home Article Heading 5</h4>
+					</div>
+					<div className='home-article__body'>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Temporibus est laborum molestiae! Sequi voluptas velit vitae ea
+							possimus recusandae? Nemo reiciendis laudantium consequatur fuga,
+							officiis facere qui dolorem quia earum?
+						</p>
+					</div>
+				</article>
+			</section>
         </div>
     )
 }
